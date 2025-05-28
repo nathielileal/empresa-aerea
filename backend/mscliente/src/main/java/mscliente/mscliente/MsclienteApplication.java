d04349590f9f49118d24ffd679c54ef12fdb1f02
unknown <guilherme.viol1914@hotmail.com>
2025-05-27 21:34:38 -0300

    init mscliente

---
+++ backend/mscliente/src/main/java/mscliente/mscliente/MsclienteApplication.java
@@ -0,0 +1,13 @@
+package mscliente.mscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclienteApplication.class, args);
+	}
+
+}
