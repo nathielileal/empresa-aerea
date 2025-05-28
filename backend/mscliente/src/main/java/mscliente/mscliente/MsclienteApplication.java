3f6dbd07ffb97d7097fbbfa88f03094d0574bf05
unknown <guilherme.viol1914@hotmail.com>
2025-05-28 00:17:37 -0300

    mscliente-CreateCliente

--- backend/mscliente/src/main/java/mscliente/mscliente/MsclienteApplication.java
+++ backend/mscliente/src/main/java/mscliente/mscliente/MsclienteApplication.java
@@ -2,6 +2,12 @@ package mscliente.mscliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import mscliente.mscliente.DTO.ClienteDTO;
+import mscliente.mscliente.model.Cliente;
+
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class MsclienteApplication {
@@ -10,4 +16,18 @@ public class MsclienteApplication {
 		SpringApplication.run(MsclienteApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+	
+		mapper.typeMap(ClienteDTO.class, Cliente.class).addMappings(map -> {
+			map.map(ClienteDTO::getEndereco, Cliente::setEndereco);
+		});
+	
+		mapper.typeMap(Cliente.class, ClienteDTO.class).addMappings(map -> {
+			map.map(Cliente::getEndereco, ClienteDTO::setEndereco);
+		});
+	
+		return mapper;
+	}
 }
