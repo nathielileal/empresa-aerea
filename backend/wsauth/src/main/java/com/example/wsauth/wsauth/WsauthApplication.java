30a4243e7eb4b25d181473813939c3e237b16473
unknown <guilherme.viol1914@hotmail.com>
2025-05-29 23:29:57 -0300

    sagaAutocadastro OK - ajustar retorno do login

--- backend/wsauth/src/main/java/com/example/wsauth/wsauth/WsauthApplication.java
+++ backend/wsauth/src/main/java/com/example/wsauth/wsauth/WsauthApplication.java
@@ -1,7 +1,9 @@
 package com.example.wsauth.wsauth;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WsauthApplication {
@@ -10,4 +12,10 @@ public class WsauthApplication {
 		SpringApplication.run(WsauthApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+
+		return mapper;
+	}
 }
